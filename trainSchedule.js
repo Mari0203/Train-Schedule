8244c64c87fdf29e8a43331d0b32e34b559aa5ff
Mari0203 <mari.brands@pm.me>
2019-06-11 00:45:16 -0500

    Added Firebase configuration file in trainSchedule.js

---
+++ trainSchedule.js
@@ -0,0 +1,15 @@
+// My Train-Scheduler web app's Firebase configuration:
+var firebaseConfig = {
+    apiKey: "AIzaSyBfjz92wYPQdfbQ10DcLjIIeTa6X5EiYXM",
+    authDomain: "train-schedules-93ab6.firebaseapp.com",
+    databaseURL: "https://train-schedules-93ab6.firebaseio.com",
+    projectId: "train-schedules-93ab6",
+    storageBucket: "",
+    messagingSenderId: "519954514054",
+    appId: "1:519954514054:web:7b7248a10bf56f5c"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  
